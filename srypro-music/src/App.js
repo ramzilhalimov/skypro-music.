dcf125b993f4d1319eaf2e045ec8d4398a9b559f
*твое <*>
2023-08-14 21:36:49 +0500

    home-1

---
+++ srypro-music/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-name">Здесь скоро будет Skypro.Music</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
