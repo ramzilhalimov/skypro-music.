75340d3cca07715b429bacdbd59e3be152536ebb
*твое <*>
2023-09-20 19:00:32 +0500

    home-6 new

--- srypro-music/src/components/NavMenu/NavMenuStyle.js
+++ srypro-music/src/components/NavMenu/NavMenuStyle.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const MainNav = styled.nav`
   width: 244px;
-  background-color: #383838;
+  background-color: #181818;
   padding: 20px 0 20px 36px;
 `
 
